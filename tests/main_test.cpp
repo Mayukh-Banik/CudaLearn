8cdf223e41cc2b16c6d68269ee93324b41049b95
Mayukh <mayukh1banik@gmail.com>
2024-12-24 23:33:48 -0500

    Integrated Gtest, testing C framework setup

--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -1,8 +1,12 @@
+#include <gtest/gtest.h>
 #include "temp.h"
-#include <iostream>
 
-int main()
-{
-    std::cout << (add(1, 1) == 2 ? "Test Passed" : "Test Failed") << std::endl;
-    return 0;
+// Example test case
+TEST(SimpleTest, 1plus1) {
+    EXPECT_EQ(add(1,1), 2);  // Test that 1 + 1 equals 2
+}
+
+// Another test case
+TEST(SimpleTest, 3plus3) {
+    EXPECT_EQ(add(3,3), 6);  // Test that 2 * 3 equals 6
 }
